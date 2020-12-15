d9325827ef5d114fda3b2f7a70af4f5e9fa265bc
Ben Altman <altmanguitar@gmail.com>
2020-12-15 03:13:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,7 +32,7 @@ class Song
   def self.genre_count
     hash = {}
     @@genres.each do |genre|
-    binding.pry
+#    binding.pry
       if !hash.contains? genre, hash[genre] += 1
       else hash[genre] = 1
       end
