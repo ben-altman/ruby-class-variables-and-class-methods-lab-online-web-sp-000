b437e3c672f18be4cacda68f34b5533e024f1184
Ben Altman <altmanguitar@gmail.com>
2020-12-15 04:39:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,4 +42,16 @@ class Song
     hash
   end
 
+  def self.artist_count
+    hash = {}
+    @@artists.each do |artist|
+  #    binding.pry
+      if hash[artist]
+        hash[artist] += 1
+      else
+        hash[artist] = 1
+      end
+    end
+    hash
+  end
 end
